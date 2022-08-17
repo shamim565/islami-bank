abe5b2149f064426603d7bef3f5d9df3337bc266
Shamim Azad <39885643+shamim565@users.noreply.github.com>
2022-08-17 17:14:43 +0600

    validation added

--- deposit.js
+++ deposit.js
@@ -1,25 +1,52 @@
 document.getElementById('deposit-btn').addEventListener('click', function () {
 
-    const depositAmount = document.getElementById('deposit-amount');
-    const totalDeposit = document.getElementById('total-deposit');
-    const deposit = parseFloat(totalDeposit.innerText) + parseFloat(depositAmount.value);
-    totalDeposit.innerText = deposit;
 
-    const totalBalance = document.getElementById('total-balance');
-    const balance = parseFloat(totalBalance.innerText) + parseFloat(depositAmount.value);
-    totalBalance.innerText = balance;
-    depositAmount.value = '';
+    const depositAmount = document.getElementById('deposit-amount');
+    if (isNaN(parseFloat(depositAmount.value))) {
+        alert('Please provide amount in number. ex. 1200');
+        depositAmount.value = '';
+        return;
+    }
+    else {
+        const totalDeposit = document.getElementById('total-deposit');
+        const deposit = parseFloat(totalDeposit.innerText) + parseFloat(depositAmount.value);
+        console.log(parseFloat(depositAmount.value));
+
+        totalDeposit.innerText = deposit;
+
+        const totalBalance = document.getElementById('total-balance');
+        const balance = parseFloat(totalBalance.innerText) + parseFloat(depositAmount.value);
+        totalBalance.innerText = balance;
+        depositAmount.value = '';
+    }
+
+
 });
 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawAmount = document.getElementById('withdraw-amount');
-    const totalWithdraw = document.getElementById('total-withdraw');
-    const withdraw = parseFloat(totalWithdraw.innerText) + parseFloat(withdrawAmount.value);
-    totalWithdraw.innerText = withdraw;
+    if (isNaN(parseFloat(withdrawAmount.value))) {
+        alert('Please provide amount in number. ex. 1200');
+        withdrawAmount.value = '';
+        return;
+    }
+    else {
+        const totalWithdraw = document.getElementById('total-withdraw');
+        const prevWithdraw = parseFloat(totalWithdraw.innerText);
+        const newWithdraw = parseFloat(withdrawAmount.value);
+        const withdraw = prevWithdraw + newWithdraw;
 
-    const totalBalance = document.getElementById('total-balance');
-    const balance = parseFloat(totalBalance.innerText) - parseFloat(withdrawAmount.value);
-    totalBalance.innerText = balance;
-    withdrawAmount.value = '';
+        const totalBalance = document.getElementById('total-balance');
+        const balance = parseFloat(totalBalance.innerText) - parseFloat(withdrawAmount.value);
+        withdrawAmount.value = ''
+        if (newWithdraw > parseFloat(totalBalance.innerText)) {
+            alert("Insufficient Balance!");
+            return
+        }
+        totalWithdraw.innerText = withdraw;
+        totalBalance.innerText = balance;
+
+    }
+
 });
 
